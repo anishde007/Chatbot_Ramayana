fe99033c71e859ecd671d904bdb518a6df02fdd3
Anish De <anishde@users.noreply.huggingface.co>
2023-12-31 14:40:24 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain import HuggingFaceHub
 
+spi=os.environ.get('spi')
+
 COUNT, N = 0, 0
 chat_history = []
 chain = ''
